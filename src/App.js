35440ba14fe8d73599b1848dd98f48b4302b13d6
Skyler Trent <skyler.trent93@gmail.com>
2022-06-23 17:16:52 -0600

    practicing apis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import react, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coins">
+      <div className="search"></div>
+      <h1 className="text">Search crypto</h1>
+      <form>
+        <input type="text" placeholder="Search" className="input"></input>
+      </form>
     </div>
   );
 }
